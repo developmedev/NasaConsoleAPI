3890b080c2cb82cf34e01ecff322d81e84d599a5
developmedev <41745903+developmedev@users.noreply.github.com>
2018-12-07 12:53:52 +0200

    Uploaded Base version

---
+++ NasaAPIProject/Nasa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace NasaAPIProject
+{
+    class Nasa
+    {
+        public Nasa()
+        {
+
+        }
+
+        public string GetTest()
+        {
+            try
+            {
+                var api_key = "DqOLnNdvyAucOxwwvjDQ4XQGd2Wl7jtB368Q61rX";
+                var client = new RestClient("https://api.nasa.gov/planetary/apod?api_key="+api_key);
+                var request = new RestRequest();
+                request.Method = Method.GET;
+              
+                var response = client.Execute(request);
+                var content = response.Content; // raw content as string  
+                return content;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+              
+            }
+        }
+
+    }
+}
---
+++ NasaAPIProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAPIProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nasa nasa = new Nasa();
+            Console.WriteLine(nasa.GetTest());
+            Console.ReadKey();
+        }
+    }
+}
